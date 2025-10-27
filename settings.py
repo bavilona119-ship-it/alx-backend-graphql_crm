298e31644d67809c531a7c15ab6305294645aff0
bavilona119-ship-it <bavilona119@gmail.com>
2025-10-27 18:39:36 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,17 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'graphene_django',
+    'crm',
+]
+
+GRAPHENE = {
+    "SCHEMA": "alx_backend_graphql_crm.schema.schema"
+}
+
+# Usual Django configurations below
 
