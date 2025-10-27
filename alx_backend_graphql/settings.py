778f505d26cd66e2acd07ef230cbc82f95db266f
bavilona119-ship-it <bavilona119@gmail.com>
2025-10-27 20:33:04 +0300

    Create settings.py

---
+++ alx_backend_graphql/settings.py
@@ -0,0 +1,11 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'graphene_django',
+    'crm',
+    'django_filters',  # ✅ Added
+]
