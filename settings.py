6314c05dd6aad3ce6c62d608e283df18bdcd2794
bavilona119-ship-it <bavilona119@gmail.com>
2025-10-27 20:22:38 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,6 +13,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',  # ✅ Added
 
     # ✅ Required apps
     'graphene_django',
